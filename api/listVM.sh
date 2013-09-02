a438f570bc859bc164f95718a33552c3b64e656b
Jazz Wang <jazzwang.tw@gmail.com>
2013-09-03 01:26:13 +0800

    * check the class files before executing java

--- api/listVM.sh
+++ api/listVM.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+if [ ! -f listVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
 java -cp .:./lib/cboss-hws-sdk-1.0.0.jar listVM > listVM.log 2>/dev/null
 cat listVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O listVM.json
 rm listVM.log
--- api/startVM.sh
+++ api/startVM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $1
+if [ ! -f startVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
 java -cp .:./lib/cboss-hws-sdk-1.0.0.jar startVM $1 > startVM.log 2> /dev/null
 cat startVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O startVM.json
 rm startVM.log
