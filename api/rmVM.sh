4dee167a73b7c4dc9b7f32e8c173e300133ba8d5
Jazz Wang <jazzwang.tw@gmail.com>
2013-09-03 20:43:36 +0800

    * add support for remove VMs

---
+++ api/rmVM.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ ! -f rmVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
+java -cp .:./lib/cboss-hws-sdk-1.0.0.jar rmVM $1 > rmVM.log 2> /dev/null
+cat rmVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O rmVM.json
+rm rmVM.log
+cat rmVM.json
