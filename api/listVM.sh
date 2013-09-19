6fc0aa85ce29664b78dfcab55f63f6c43f48b496
Jazz Wang <jazzwang.tw@gmail.com>
2013-09-19 22:28:22 +0800

    * update API scripts

--- api/listVM.sh
+++ api/listVM.sh
@@ -2,5 +2,4 @@
 if [ ! -f listVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
 java -cp .:./lib/cboss-hws-sdk-1.0.0.jar listVM > listVM.log 2>/dev/null
 cat listVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O listVM.json
-rm listVM.log
-cat listVM.json | sed 's#,#\n#g' | grep "instanceId" | sed 's#.*:"##' | tr -d '"'
+cat listVM.json | sed 's#,#\n#g' | sed 's#:# #' | tr -d '"' | awk '/operationStatus/ { A=$2 } /instanceId/ { B=$2 } /name/ { C=$2 } /provisionStatus/ { print B,A,C }'
--- api/startVM.sh
+++ api/startVM.sh
@@ -2,5 +2,5 @@
 if [ ! -f startVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
 java -cp .:./lib/cboss-hws-sdk-1.0.0.jar startVM $1 > startVM.log 2> /dev/null
 cat startVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O startVM.json
-rm startVM.log
 cat startVM.json
+printf "\n"
