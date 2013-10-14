88a8227349fd626f8034a18d2b6046fd05df058f
Jazz Wang <jazzwang.tw@gmail.com>
2013-10-14 20:39:59 +0800

    * tiny changes to show the result of rmVM.sh

--- api/rmVM.sh
+++ api/rmVM.sh
@@ -3,3 +3,4 @@ if [ ! -f rmVM.class ]; then echo "run 'ant' to build required class files first
 java -cp .:./lib/cboss-hws-sdk-1.0.0.jar rmVM $1 > rmVM.log 2> /dev/null
 cat rmVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O rmVM.json
 cat rmVM.json
+printf "\n"
