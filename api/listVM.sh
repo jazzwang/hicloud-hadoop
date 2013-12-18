96228dabe06e870aaa463fa46a4574ff8d430c7d
Jazz Wang <jazzwang.tw@gmail.com>
2013-12-18 15:44:26 +0800

    * add software dependency to install-ubuntu  * tiny modified for listVM.sh output format

--- api/listVM.sh
+++ api/listVM.sh
@@ -2,4 +2,4 @@
 if [ ! -f listVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
 java -cp .:./lib/cboss-hws-sdk-1.0.0.jar listVM > listVM.log 2>/dev/null
 cat listVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O listVM.json
-cat listVM.json | sed 's#,#\n#g' | sed 's#:# #' | tr -d '"[{' | awk '/operationStatus/ { A=$2 } /instanceId/ { B=$2 } /name/ { C=$2 } /applyDate/ { D=$2 } /ipForVm/ { E=$2 } /provisionStatus/ { print B,A,C,E,D }' | column -t
+cat listVM.json | sed 's#,#\n#g' | sed 's#:# #' | tr -d '"[{}]' | awk '/operationStatus/ { A=$2 } /instanceId/ { B=$2 } /name/ { C=$2 } /ipForVm/ { D=$2 } /startDate/ { E=$2 ; print B,A,C,D,E } ' | column -t
