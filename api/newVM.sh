8592bcd2fba4670e77a8f7d06ba0092018bab5a5
Jazz Wang <jazzwang.tw@gmail.com>
2013-12-27 16:26:08 +0800

    * upgrade to hadoop 1.2.1

--- api/newVM.sh
+++ api/newVM.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ ! -f newVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
 java -cp .:./lib/cboss-hws-sdk-1.0.0.jar newVM $1 > newVM.log 2> /dev/null
-cat newVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O newVM.json
+#cat newVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O newVM.json
