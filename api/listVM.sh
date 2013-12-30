3e6c4c335adea2133b9001c0b9c1e830269d2c97
Jazz Wang <jazzwang.tw@gmail.com>
2013-12-30 21:38:06 +0800

    * modified for new CaaS API jar file

--- api/listVM.sh
+++ api/listVM.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 if [ ! -f listVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
-java -cp .:./lib/cboss-hws-sdk-1.0.0.jar listVM > listVM.json 2>/dev/null
-#cat listVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O listVM.json
-cat listVM.json | sed 's#,#\n#g' | sed 's#:# #' | tr -d '"[{}]' | awk '/operationStatus/ { A=$2 } /instanceId/ { B=$2 } /name/ { C=$2 } /ipForVm/ { D=$2 } /startDate/ { E=$2 ; print B,A,C,D,E } ' | column -t
+java -cp .:./lib/cboss-hws-sdk-1.0.0.jar listVM > listVM.log 2>/dev/null
+cat listVM.log | grep "Result={" | sed 's#.*Result=##' | sed 's#,#\n#g' | sed 's#:# #' | tr -d '"[{}]' | awk '/operationStatus/ { A=$2 } /instanceId/ { B=$2 } /name/ { C=$2 } /ipForVm/ { D=$2 } /startDate/ { E=$2 ; print B,A,C,D,E } ' | column -t
--- api/startVM.sh
+++ api/startVM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f startVM.class ]; then echo "run 'ant' to build required class files first."; exit -1; fi
-java -cp .:./lib/cboss-hws-sdk-1.0.0.jar startVM $1 > startVM.log 2> /dev/null
+java -cp .:./lib/cboss-hws-sdk-1.0.0.jar startVM $1 >> startVM.log 2> /dev/null
 #cat startVM.log | grep "URL=" | sed 's#.*URL=##g' | xargs wget -q --no-check-certificate -O startVM.json
 #cat startVM.json
 #printf "\n"
